7220ddd92e01ecfc97696c230f0fb82a2d29ca38
Ernst-D <ernst.dzerevanko@gmail.com>
2024-02-07 17:16:25 +0400

    comment regarding timeouts

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,13 @@ const { defineConfig, devices } = require('@playwright/test');
 const config = {
 
   testDir: './tests',
+  /**
+   * if you'd like to remain long test timeout - better to bind on
+   * some environmental variable like DEBUG, if DEBUG=1 (for example) - 
+   * you're going to have 500 seconds timeout but not 50.
+   * 
+   * Example: `timeout: process.env.DEBUG ? 50 * 10000 : 50 * 1000`
+   */
   timeout: 50 * 10000,
  
   expect:{
